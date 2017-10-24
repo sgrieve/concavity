4b4f4103a1f5a14b73cec459cfebec12f02e68e5
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-10-24 16:30:29 +0100

    working polygon interesction test ready for real data

--- get_dl_list.py
+++ get_dl_list.py
@@ -8,3 +8,9 @@ import json
 # if this is horrifyingly slow (it will be), it can be run on an as needed
 # basis for each file to be processed rather than all at once, but it might
 # be worth just running this step on legion
+
+from shapely.geometry.polygon import Polygon
+
+polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+poly2 = Polygon([(0, 0), (0, 1), (-1, 1), (-1, 0)])
+print(polygon.intersects(poly2))
