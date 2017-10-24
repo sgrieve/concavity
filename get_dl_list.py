4bb459d760e20511e2daebaebf18642f1b743464
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-10-24 16:16:36 +0100

    new script file with notes on generating the download list

---
+++ get_dl_list.py
@@ -0,0 +1,10 @@
+import json
+
+# load all of the srtm coords and loop thru the key value pairs
+# for each coord, test if the point is within the cz polygon
+# if it is, store the dict key in a new list of tiles to dl
+# write this list to an output dict where the key is the cz filename
+
+# if this is horrifyingly slow (it will be), it can be run on an as needed
+# basis for each file to be processed rather than all at once, but it might
+# be worth just running this step on legion
