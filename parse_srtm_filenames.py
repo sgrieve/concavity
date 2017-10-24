aa3c32d262728b87861ff30fef35b5fcfb2bf817
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-10-24 16:01:29 +0100

    code is now parsing the coordinates of each tile

--- parse_srtm_filenames.py
+++ parse_srtm_filenames.py
@@ -1,5 +1,3 @@
-import re
-
 with open('srtm_filenames.lst', 'r') as f:
     srtm = f.readlines()
 
@@ -7,3 +5,21 @@ for s in srtm:
     short = s.split('/')[-1].split('.')[0]
     left = short[:3]
     right = short[3:]
+
+    if left.startswith('N'):
+        left = float(left[1:])
+    elif left.startswith('S'):
+        left = float(left[1:]) * -1
+
+    if right.startswith('E'):
+        right = float(right[1:])
+    elif right.startswith('W'):
+        right = float(right[1:]) * -1
+
+    # get the 4 points, clockwise from bottom left (lat, long)
+    p1 = (left, right)
+    p2 = (left + 1, right)
+    p3 = (left + 1, right + 1)
+    p4 = (left, right + 1)
+
+    final = (p1, p2, p3, p4)
