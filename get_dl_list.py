6ad626697f0a37052f30bd4a8d0b1b5f6927b620
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-10-25 11:08:58 +0100

    Now generating the download links for each cz polygon as a json

--- get_dl_list.py
+++ get_dl_list.py
@@ -1,16 +1,39 @@
 import json
-
-# load all of the srtm coords and loop thru the key value pairs
-# for each coord, test if the point is within the cz polygon
-# if it is, store the dict key in a new list of tiles to dl
-# write this list to an output dict where the key is the cz filename
-
-# if this is horrifyingly slow (it will be), it can be run on an as needed
-# basis for each file to be processed rather than all at once, but it might
-# be worth just running this step on legion
-
 from shapely.geometry.polygon import Polygon
 
-polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
-poly2 = Polygon([(0, 0), (0, 1), (-1, 1), (-1, 0)])
-print(polygon.intersects(poly2))
+
+def bounds_to_poly(bbox):
+    '''
+    Helper function to go from lat and long max and min values to a shapely
+    polygon
+    '''
+    return Polygon(((bbox[1], bbox[3]), (bbox[0], bbox[3]),
+                   (bbox[0], bbox[2]), (bbox[1], bbox[2])))
+
+results = {}
+
+base_url = 'https://cloud.sdsc.edu/v1/AUTH_opentopography/Raster/SRTM_GL1/'
+
+
+with open('srtm_coords.json') as srtm:
+    srtm_data = json.load(srtm)
+
+with open('bboxes.json') as cz:
+    cz_coords = json.load(cz)
+
+no_of_cz = len(cz_coords) - 1
+
+for i, (cz_filename, cz_data) in enumerate(cz_coords.items()):
+    print('Processing', i, 'of', no_of_cz)
+    tmp_results = []
+    cz_poly = bounds_to_poly(cz_data['bbox'])
+
+    for srtm_filename, srtm_coords in srtm_data.items():
+        srtm_poly = Polygon(srtm_coords)
+        if srtm_poly.intersects(cz_poly):
+            tmp_results.append(base_url + srtm_filename)
+    results[cz_filename] = tmp_results
+
+
+with open('download_links.json', 'w') as f:
+    json.dump(results, f, sort_keys=True, indent=4)
