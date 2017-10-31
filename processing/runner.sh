1cac385549c127efea6adaf0042ae1a7dde8823d
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-10-31 09:00:44 +0000

    suppressing output from wget, converting bils to float32 and removing the vrt files after use

--- processing/runner.sh
+++ processing/runner.sh
@@ -6,14 +6,15 @@
 
 cd /home/ccearie/Scratch/SRTM/
 
-python /home/ccearie/concavity/processing/get_urls.py $1.shp | xargs wget
+python /home/ccearie/concavity/processing/get_urls.py $1.shp | xargs wget -q
 
 gdalwarp --config GDAL_CACHEMAX 3000 -wm 3000 -srcnodata -32768 -dstnodata -9999 -cutline /home/ccearie/concavity/climate_zones/singlepart_files/$1.shp -crop_to_cutline -of ENVI *.hgt tmp.bil
 
-gdalwarp -t_srs '+proj=utm +zone='$2' +datum=WGS84 +'$3'' -of ENVI tmp.bil $1.bil
+gdalwarp -t_srs '+proj=utm +zone='$2' +datum=WGS84 +'$3'' -of ENVI -ot Float32 tmp.bil $1.bil
 
 rm tmp.*
 rm *.hgt
+rm *.vrt
 
 # Run the LSD code (on legion)
 /home/ccearie/LSD/LSDTopoTools_ChiMudd2014-master/driver_functions_MuddChi2014/chi_mapping_tool.exe  /home/ccearie/concavity/processing/ SRTM.driver /home/ccearie/Scratch/SRTM/ $1 /home/ccearie/Scratch/SRTM/ $1
