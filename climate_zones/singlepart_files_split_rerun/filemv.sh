327f94fef844139b4381f0a9300de8d783215a82
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-12-01 23:29:41 +0000

    adding double split polygons

---
+++ climate_zones/singlepart_files_split_rerun/filemv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp ../singlepart_files_split/25_102_ada76b9b_0625_410d_bde6_824263252ff5.* .
+cp ../singlepart_files_split/25_102_1e28bc13_e3fe_4a62_8737_636e3312528e.* .
+cp ../singlepart_files_split/25_44_2aec770e_d59c_43bb_b1a6_6bb27f90f398.* .
+cp ../singlepart_files_split/25_102_d018e04f_a790_4963_9cfc_3062fe4c8e18.* .
+cp ../singlepart_files_split/25_102_7518de3a_25c4_4ca3_b736_5ed0e4c79e4e.* .
+cp ../singlepart_files_split/25_102_5c70f84a_7cc0_49af_a018_c2bcdc9dfcdc.* .
+cp ../singlepart_files_split/21_9.* .
+cp ../singlepart_files_split/17_26.* .
+cp ../singlepart_files_split/25_102_aa5fd30e_9601_4eeb_abc2_70339496f513.* .
+cp ../singlepart_files_split/25_102_9eec1bf9_f725_4417_a4e6_9c3bb9970660.* .
+cp ../singlepart_files_split/25_102_071b7fc2_3a23_4ed0_9a5d_054b3295a75f.* .
+cp ../singlepart_files_split/25_102_1a23c5b2_7ba1_444f_86ba_63d63dd8f376.* .
+cp ../singlepart_files_split/25_102_53c5c657_0e1a_4257_bf63_da1f9646c214.* .
+cp ../singlepart_files_split/25_102_0612589f_1410_46df_aa7c_93a5847f3d53.* .
